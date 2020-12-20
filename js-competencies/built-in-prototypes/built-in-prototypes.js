4a46c9377c8a3e412f101241fa65e3c15f353ebc
Steven Larsen <slarsen0823@gmail.com>
2020-12-20 16:44:21 -0700

    built-in-prototypes finished

--- js-competencies/built-in-prototypes/built-in-prototypes.js
+++ js-competencies/built-in-prototypes/built-in-prototypes.js
@@ -3,11 +3,11 @@
 
 //////////////////PROBLEM 1////////////////////
 
-//Write a function called dragonMapper that takes in an 
-//array and maps over each element, adding the string 
-//'...here be dragons' to the end of each element. 
-//dragonMapper should return the the new array. You should
-//use the built-in array method .map
+// Write a function called dragonMapper that takes in an 
+// array and maps over each element, adding the string 
+// '...here be dragons' to the end of each element. 
+// dragonMapper should return the the new array. You should
+// use the built-in array method .map
   
   //Code here
 function dragonMapper(arr, i){
@@ -17,41 +17,41 @@ function dragonMapper(arr, i){
 
 //////////////////PROBLEM 2////////////////////
 
-//Write a function called cookieLoversOnly that takes 
-//in an array and filters out every object that
-//does NOT have the key favoriteCookie.
-//cookieLoversOnly should return the filtered array.
+// Write a function called cookieLoversOnly that takes 
+// in an array and filters out every object that
+// does NOT have the key favoriteCookie.
+// cookieLoversOnly should return the filtered array.
   
   //Code here
 function cookieLoversOnly(arr) {
-  return arr.filter(obj => obj.key === favoriteCookie)
+  return arr.filter(obj => obj.favoriteCookie)
 }
 
 //////////////////PROBLEM 3////////////////////
 
-//Write a function called changeValue that takes 
-//in an array, and a callback function.
-//changeValue should invoke the callback function 
-//for every element in the array. 
-//changeValue should pass the current element into 
-//the callback function as an argument every time it is invoked.
-//Hint: there is an array method for this.
+// Write a function called changeValue that takes 
+// in an array, and a callback function.
+// changeValue should invoke the callback function 
+// for every element in the array. 
+// changeValue should pass the current element into 
+// the callback function as an argument every time it is invoked.
+// Hint: there is an array method for this.
 
 
   //Code here
   function changeValue(arr, cb) {
-    
+    arr.forEach(e => cb(e))
   }
 
 
 
 //////////////////PROBLEM 4////////////////////
 
-//Write a function called findProduct that takes in 
-//an array, and uses .reduce to find the product of 
-//the elements (here the word product means the result 
-//of multiplying all the elements together)
-//Return the product.
+// Write a function called findProduct that takes in 
+// an array, and uses .reduce to find the product of 
+// the elements (here the word product means the result 
+// of multiplying all the elements together)
+// Return the product.
   
   //Code here
 function findProduct(arr) {
@@ -61,12 +61,12 @@ function findProduct(arr) {
 
 //////////////////PROBLEM 5////////////////////
 
-//Write a function called cipherize that takes in 
-//an array, and a string. It should use the 
-//array method .indexOf to find the index of the passed-in 
-//string in the array. Then, it should use the method .charAt
-//to find the character at that index, in the string. 
-//Return that character.
+// Write a function called cipherize that takes in 
+// an array, and a string. It should use the 
+// array method .indexOf to find the index of the passed-in 
+// string in the array. Then, it should use the method .charAt
+// to find the character at that index, in the string. 
+// Return that character.
 
 //EX: cipherize(["hello", "world", "wide", "web"], "wide")
 // index of string in the array: 2
@@ -77,7 +77,7 @@ function findProduct(arr) {
 function cipherize(arr, str) {
   let string = arr.indexOf(str);
   console.log(string)
-  let character = string.charAt;
+  let character = str.charAt(string);
   console.log(character)
   return character
 }
@@ -85,10 +85,10 @@ function cipherize(arr, str) {
 
 //////////////////PROBLEM 6////////////////////
 
-//Write a function called reverseMe that takes in 
-//a string, reverses it, and returns the reversed 
-//string. Use built-in methods for taking the string 
-//apart, reversing it, and putting it back together again.
+// Write a function called reverseMe that takes in 
+// a string, reverses it, and returns the reversed 
+// string. Use built-in methods for taking the string 
+// apart, reversing it, and putting it back together again.
 
   //Code here
 function reverseMe(str) {
