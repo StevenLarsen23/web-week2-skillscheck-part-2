8fdc9a578204e4f37a1c895887f404405b95178c
Steven Larsen <slarsen0823@gmail.com>
2020-12-26 11:26:23 -0700

    Data-types completed

--- js-competencies/data-types/data-types.js
+++ js-competencies/data-types/data-types.js
@@ -1,154 +1,166 @@
 
 ///////////////////Problem 1//////////////////
-//Uncomment the array that contains valid JavaScript datatypes
+// Uncomment the array that contains valid JavaScript datatypes
 
 // var jsDataTypes = ["bool", "string", "object", "integer", "function", "undefined"]
 // var jsDataTypes = ["boolean", "string", "object", "int", "function"]
 // var jsDataTypes = ["bool", "string", "object", "integer", "function"]
-// var jsDataTypes = ["boolean", "string", "object", "number", "function", "undefined"]
+var jsDataTypes = ["boolean", "string", "object", "number", "function", "undefined"]
 
 
 ///////////////////Problem 2//////////////////
-//Change the value of the variable iLikeChocolate from
-//null to a boolean - based on whether or not you like 
-//chocolate!
+// Change the value of the variable iLikeChocolate from
+// null to a boolean - based on whether or not you like 
+// chocolate!
 
-var iLikeChocolate = null
+var iLikeChocolate = true
 
 
 ///////////////////Problem 3//////////////////
-//Create a string, and assign it to a variable
-//called myString
+// Create a string, and assign it to a variable
+// called myString
 
-//Code here
+let myString = 'this is my string'
 
 
 ///////////////////Problem 4//////////////////
-//Create an integer, and assign it to a variable
-//called myInt
+// Create an integer, and assign it to a variable
+// called myInt
 
-//Code here
+let myInt = 23
 
 
 ///////////////////Problem 5//////////////////
-//Create a function called myFunction that returns
-//the string "I am a function!"
+// Create a function called myFunction that returns
+// the string "I am a function!"
 
-//Code here
-
+function myFunction() {
+    return 'I am a function!'
+}
 
 ///////////////////Problem 6//////////////////
-//Create an object called myObject that has a 
-//name property (a string), a team property (a string),
-//and a method called whatTeam that returns the team
-//property.
+// Create an object called myObject that has a 
+// name property (a string), a team property (a string),
+// and a method called whatTeam that returns the team
+// property.
 
-//Code here
+let myObject = {
+    name: 'Batman',
+    team: 'Justice League',
+    whatTeam(){
+        return this.team
+    } 
+}
 
 
 ///////////////////Problem 7//////////////////
-//Create an array called myArray that contains:
-//1, "two", { three: 3 }, and as the 4th element 
-//an array literal that contains 4, 5
+// Create an array called myArray that contains:
+// 1, "two", { three: 3 }, and as the 4th element 
+// an array literal that contains 4, 5
 
-//Code here
+let myArray = [1, 'two', {three: 3}, new Array(4, 5)]
 
 
 ///////////////////Problem 8//////////////////
-//Make a function called whatAmI that takes in one 
-//parameter, a mystery parameter. whatAmI should
-//return the data type of the mystery parameter.
+// Make a function called whatAmI that takes in one 
+// parameter, a mystery parameter. whatAmI should
+// return the data type of the mystery parameter.
 
-//Code here
+function whatAmI(param){
+    return typeof(param)
+}
 
 
 ///////////////////Problem 9//////////////////
-//Create a variable called theVoid. Make it null.
+// Create a variable called theVoid. Make it null.
 
-//Code here
+let theVoid = null
 
 
 ///////////////////Problem 10//////////////////
-//Create a function called undefiner that takes in 
-//an array, and sets the 2nd element of that array
-//to undefined. undefiner should return the array.
+// Create a function called undefiner that takes in 
+// an array, and sets the 2nd element of that array
+// to undefined. undefiner should return the array.
 
-//Code here
+function undefiner(arr){
+    arr.splice(1, 1, undefined)
+    return arr
+}
 
 
 ///////////////////Problem 11//////////////////
-//Create a function called nananan that takes in 
-//a parameter. If the parameter divided by 2 is NaN, 
-//return "FLYING NOCTURNAL MAMMAL MAN!". Otherwise, return the result of
-//the parameter divided by 2.
+// Create a function called nananan that takes in 
+// a parameter. If the parameter divided by 2 is NaN, 
+// return "FLYING NOCTURNAL MAMMAL MAN!". Otherwise, return the result of
+// the parameter divided by 2.
 
-//Code here
+let nananan = (param) => isNaN(param / 2) ? "FLYING NOCTURNAL MAMMAL MAN!" : param / 2
 
 
-//Now, call nananan with the value NaN, and store the 
-//result of the function call in a variable called "na".
+// Now, call nananan with the value NaN, and store the 
+// result of the function call in a variable called "na".
 
-//Code here
+let na = nananan(NaN)
 
 
 ///////////////////Problem 12//////////////////
-//Create a function called arrayChecker that takes 
-//in a parameter. arrayChecker should return true
-//if the parameter is an array, and false otherwise.
+// Create a function called arrayChecker that takes 
+// in a parameter. arrayChecker should return true
+// if the parameter is an array, and false otherwise.
 
-//Code here
+let arrayChecker = (param) => Array.isArray(param) ? true : false
 
 
 ///////////////////Problem 13//////////////////
-//Create a function called numFromString that takes 
-//in a string. numFromString should use the parseInt
-//function to convert the string into a base 10 number.
-//Return that base 10 number.
+// Create a function called numFromString that takes 
+// in a string. numFromString should use the parseInt
+// function to convert the string into a base 10 number.
+// Return that base 10 number.
 
-//Code here
+let numFromString = (str) => parseInt(str)
 
 
 ///////////////////Problem 14//////////////////
-//Create a function called makeItAString that takes 
-//in a parameter. makeItAString should use the toString
-//function to convert the parameter into a string.
-//Return that string.
+// Create a function called makeItAString that takes 
+// in a parameter. makeItAString should use the toString
+// function to convert the parameter into a string.
+// Return that string.
 
-//Code here
+let makeItAString = (param) => param.toString() 
 
 
 ///////////////////Problem 15//////////////////
-//Uncomment the correct answers below
+// Uncomment the correct answers below
 
-//What will 100 || 0 be evaluted to?
-// var answer1 = 100
+// What will 100 || 0 be evaluted to?
+var answer1 = 100
 // var answer1 = 0
 
-//What will "Hello" && "Goodbye" be evaluted to?
+// What will "Hello" && "Goodbye" be evaluted to?
 // var answer2 = "Hello"
-// var answer2 = "Goodbye"
+var answer2 = "Goodbye"
 
-//What will 14 + "" be evaluted to?
-// var answer3 = "14"
+// What will 14 + "" be evaluted to?
+var answer3 = "14"
 // var answer3 = 14
 
-//What will "14" - 10 be evaluted to?
+// What will "14" - 10 be evaluted to?
 // var answer4 = "14"
 // var answer4 = "1410"
-// var answer4 = 4
+var answer4 = 4
 // var answer4 = 1410
 
-//What will ["anne", "bob"] + ["charlie"] be evaluted to?
+// What will ["anne", "bob"] + ["charlie"] be evaluted to?
 // var answer5 = ["anne", "bob", "charlie"]
 // var answer5 = "annebobcharlie"
-// var answer5 = "anne,bobcharlie"
+var answer5 = "anne,bobcharlie"
 // var answer5 = "anne,bob,charlie"
 
 
 
 ///////////////////Problem 16//////////////////
-//Uncomment the correct answers below, based the 
-//following code:
+// Uncomment the correct answers below, based the 
+// following code:
 
 var myArr = [1, 2, 3, 4]
 var myStr = "Hello, world!"
@@ -159,27 +171,27 @@ var myCopiedStr = myStr
 myCopiedArr[1] = 25
 myCopiedStr += " What a beautiful day!"
 
-//What is the value of myArr?
+// What is the value of myArr?
 // var valMyArr = [1, 2, 3, 4]
-// var valMyArr = [1, 25, 3, 4]
+var valMyArr = [1, 25, 3, 4]
 
-//What is the value of myStr?
-// var valMyStr = "Hello, world!"
+// What is the value of myStr?
+var valMyStr = "Hello, world!"
 // var valMyStr = "Hello, world! What a beautiful day!"
 
-//What is the value of myCopiedArr?
+// What is the value of myCopiedArr?
 // var valMyCopiedArr = [1, 2, 3, 4]
-// var valMyCopiedArr = [1, 25, 3, 4]
+var valMyCopiedArr = [1, 25, 3, 4]
 
-//What is the value of myCopiedStr?
+// What is the value of myCopiedStr?
 // var valMyCopiedStr = "Hello, world!"
-// var valMyCopiedStr = "Hello, world! What a beautiful day!"
+var valMyCopiedStr = "Hello, world! What a beautiful day!"
 
-//Are arrays copied by reference, or by value?
-// var arraysCopiedBy = "reference"
+// Are arrays copied by reference, or by value?
+var arraysCopiedBy = "reference"
 // var arraysCopiedBy = "value"
 
-//Are string copied by reference, or by value?
+// Are string copied by reference, or by value?
 // var stringsCopiedBy = "reference"
-// var stringsCopiedBy = "value"
+var stringsCopiedBy = "value"
 
